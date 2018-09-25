9855b79e470c8a9b67b2b97ad64a02902f1a6d25
Jordy Pereira <jordy.pereira@hotmail.com>
2018-09-25 19:46:15 +0200

    Made Descriptions compact

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="flex flex-col items-center">
-    <div class="text-orange  w-full flex flex-row justify-between mt-3">
-      <ul class="list-reset flex flex-row flex-wrap w-2/3">
-        <li v-for="(tech, i) in project.tech" :key="i" class="inline px-2 text-sm">{{ tech }}</li>
-      </ul>
-      <span class="font-semibold mr-5">{{ project.category }}</span>
-    </div>
-    <div class="px-4 py-2 md:w-2/3 mt-5">
-      <h1 class="text-center">{{ project.title }}</h1>
-    </div>
-    <div class=" w-2/3">
-      <p class="my-5 pb-5 text-center" v-html="project.description">
+    <div class="md:w-2/4 px-2">
+      <h1 class="mt-5">{{ project.title }}</h1>
+      <div class="text-orange w-full mt-3">
+        <span class="font-semibold mr-5">{{ project.category }}</span>
+        <ul class="list-reset inline">
+          <li v-for="(tech, i) in project.tech" :key="i" class="inline pr-2 text-sm">{{ tech }}</li>
+        </ul>
+      </div>
+      <p class="my-5" v-html="project.description">
       </p>
       <ul class="list-reset my-5 pt-5">
         <li v-for="(feature, i) in project.features" :key="i" class="mb-1"> <span class="font-bold text-orange">+ </span>{{ feature }}</li>
