bf433c309ea9e78d989695cfd8a243f6b5d6dc9b
Jordy Pereira <jordy.pereira@hotmail.com>
2018-09-25 20:15:13 +0200

    Links lower

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -13,7 +13,7 @@
       <ul class="list-reset my-5 pt-5">
         <li v-for="(feature, i) in project.features" :key="i" class="mb-1"> <span class="font-bold text-orange">+ </span>{{ feature }}</li>
       </ul>
-      <p class="mt-3">
+      <p class="pt-4">
         <a v-if="project.github" :href="project.github" target="_blank" rel="noopener" class="text-orange-dark hover:text-orange no-underline mt-1">Github</a> <br>
         <a v-if="project.demo" :href="project.demo" target="_blank" rel="noopener" class="text-orange-dark hover:text-orange no-underline">Demo / Website</a>
       </p>
