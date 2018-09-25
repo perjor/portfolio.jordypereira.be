d5ba51525f0caa354d2b01c5f280ba521973f12a
Jordy Pereira <jordy.pereira@hotmail.com>
2018-09-25 19:10:51 +0200

    a link was taking full width

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -6,18 +6,18 @@
       </ul>
       <span class="font-semibold mr-5">{{ project.category }}</span>
     </div>
-    <div class="px-4 py-2 md:w-2/3 my-5">
+    <div class="px-4 py-2 md:w-2/3 mt-5">
       <h1 class="text-center">{{ project.title }}</h1>
     </div>
-    <div class="my-5 w-2/3">
-      <p class="my-5 text-center" v-html="project.description">
+    <div class=" w-2/3">
+      <p class="my-5 pb-5 text-center" v-html="project.description">
       </p>
-      <ul class="list-reset my-5">
+      <ul class="list-reset my-5 pt-5">
         <li v-for="(feature, i) in project.features" :key="i" class="mb-1"> <span class="font-bold text-orange">+ </span>{{ feature }}</li>
       </ul>
-      <p class="flex flex-col mt-3">
-        <a v-if="project.github" :href="project.github" target="_blank" rel="noopener" class="text-orange-dark hover:text-orange no-underline mt-1">Github</a>
-        <a v-if="project.demo" :href="project.demo" target="_blank" rel="noopener" class="text-orange-dark hover:text-orange no-underline mt-1">Demo / Website</a>
+      <p class="mt-3">
+        <a v-if="project.github" :href="project.github" target="_blank" rel="noopener" class="text-orange-dark hover:text-orange no-underline mt-1 block">Github</a> <br>
+        <a v-if="project.demo" :href="project.demo" target="_blank" rel="noopener" class="text-orange-dark hover:text-orange no-underline mt-1 block">Demo / Website</a>
       </p>
     </div>
   </div>
