2c4eae4d7ee131382c04d31c4cb4e96a0c6a73d0
Jordy Pereira <jordy.pereira@hotmail.com>
2018-11-14 08:33:12 +0100

    Use imagemin to compress images

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,20 @@
+var ImageminPlugin = require('imagemin-webpack-plugin').default
+
 module.exports = {
   pwa: {
     name: 'Portfolio of Jordy Pereira',
     themeColor: '#ff5722',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: '#ff5722'
+  },
+  configureWebpack: {
+    plugins: [
+      new ImageminPlugin({
+        disable: process.env.NODE_ENV !== 'production',
+        pngquant: {
+          quality: '50'
+        }
+      })
+    ]
   }
 }
